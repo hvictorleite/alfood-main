8b82cc0aaa2b7f3adda9874ddb06d2b9810545af
Victor Hugo Marques Rosa Leite <hvictorleite@gmail.com.br>
2023-01-31 11:59:12 -0300

    Feito roteamento para admin de restaurantes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './paginas/Home';
+import AdminRestaurantes from './paginas/AdminRestaurantes';
 import VitrineRestaurantes from './paginas/VitrineRestaurantes';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
+      <Route path="/admin/restaurantes" element={<AdminRestaurantes />} />
     </Routes>
   );
 }
---
+++ src/paginas/AdminRestaurantes/index.tsx
@@ -0,0 +1,6 @@
+const AdminRestaurantes = () => {
+    // TODO: chamar lista de restaurantes
+    return (<h1>Yeah</h1>);
+}
+
+export default AdminRestaurantes;
