b12c13f09843d409ad516ce5473789c39365a6fc
Victor Hugo Marques Rosa Leite <hvictorleite@gmail.com.br>
2023-01-31 11:09:01 -0300

    Implementado GET restaurantes com paginação

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,27 +1,42 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import IPaginacao from '../../interfaces/IPaginacao';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 
 const ListaRestaurantes = () => {
 
-  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>();
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+  const [proximaPagina, setProximaPagina] = useState('');
 
   useEffect(() => {
     // obter restaurantes
-    axios.get('http://localhost:8000/api/v1/restaurantes/')
+    axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
       .then(response => {
         setRestaurantes(response.data.results);
+        setProximaPagina(response.data.next);
       })
       .catch(error => {
         console.log(error);
       });
   }, []);
 
+  const verMais = () => {
+    axios.get<IPaginacao<IRestaurante>>(proximaPagina)
+      .then(response => {
+        setRestaurantes([...restaurantes, ...response.data.results]);
+        setProximaPagina(response.data.next);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+    {proximaPagina && <button onClick={verMais}>ver mais</button>}
   </section>)
 }
 
