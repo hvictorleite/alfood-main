d6748be527e114e070f5f7f6d4caaa7a11145ae0
Victor Hugo Marques Rosa Leite <hvictorleite@gmail.com.br>
2023-02-02 11:18:20 -0300

    Criado campo de ordenação para busca

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,4 +1,4 @@
-import { Button, TextField } from '@mui/material';
+import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 import axios, { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
 import IPaginacao from '../../interfaces/IPaginacao';
@@ -19,6 +19,7 @@ const ListaRestaurantes = () => {
   const [paginaAnterior, setPaginaAnterior] = useState('');
 
   const [busca, setBusca] = useState('');
+  const [ordenacao, setOrdenacao] = useState('');
 
   useEffect(() => {
     // obter restaurantes
@@ -41,12 +42,13 @@ const ListaRestaurantes = () => {
     evento.preventDefault();
 
     const opcoes = {
-      params: {
-
-      } as IParametrosBusca
+      params: {} as IParametrosBusca
     }
     if (busca) {
       opcoes.params.search = busca;
+    }
+    if (ordenacao) {
+      opcoes.params.ordering = ordenacao;
     }
     carregarDados(`${uriBaseApi}/restaurantes/`, opcoes);
   };
@@ -54,7 +56,21 @@ const ListaRestaurantes = () => {
   return (
     <section className={style.ListaRestaurantes}>
       <form onSubmit={buscaRestaurantes}>
-        <TextField onChange={evento => setBusca(evento.target.value)} fullWidth id='buscar-restaurantes' label='Buscar restaurantes...' type='search' variant='standard'/>
+        <TextField onChange={evento => setBusca(evento.target.value)} id='buscar-restaurantes' label='Buscar restaurantes...' type='search' variant='standard' fullWidth/>
+        <FormControl sx={{minWidth: 140}}>
+          <InputLabel id='label-ordenar'>Ordenar por</InputLabel>
+          <Select
+            labelId='odernacao'
+            id='select-ordenacao'
+            value={ordenacao}
+            label='Ordenar Por'
+            onChange={evento => setOrdenacao(evento.target.value)}>
+            <MenuItem value=''>Nenhum</MenuItem>
+            <MenuItem value={'id'}>Id</MenuItem>
+            <MenuItem value={'nome'}>Nome</MenuItem>
+          </Select>
+        </FormControl>
+
         <Button type='submit' variant='outlined'>Buscar</Button>
       </form>
       <h1>Os restaurantes mais <em>bacanas</em>!</h1>
